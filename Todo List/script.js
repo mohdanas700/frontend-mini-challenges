8854109f8c347c78589fdc82b5774475a886f2ce
Mohd Anas <mohdanas.dev70@gmail.com>
2025-01-29 13:56:43 +0530

    Completed Todo

--- Todo List/script.js
+++ Todo List/script.js
@@ -4,6 +4,7 @@ const todoInput = document.getElementById("todoInput");
 const listContainer = document.getElementById("listContainer");
 const todo =  document.getElementById('todo');
 
+const noElementDiv = document.querySelector(".no-element")
 
 const initialItems = ['JavaScript', 'HTML', 'CSS', 'React', 'Angular', 'Zustand', 'NextJS', 'TypeScript'];
 
@@ -13,9 +14,18 @@ todoForm.addEventListener('submit', (event) => {
     const todoItem = todoInput.value;
     if(todoItem){
         addTodoItem(todoItem);
+        checkEmptyList();
     }
 });
 
+const checkEmptyList = () => {
+    if (listContainer.children.length === 0){
+        noElementDiv.style.display = 'block';
+        noElementDiv.style.fontSize = '2rem';
+    } else {
+        noElementDiv.style.display = 'none'
+    }
+}
 
 function addTodoItem(todoItem) {
     const todoElement = todo.content.cloneNode(true);
@@ -25,6 +35,37 @@ function addTodoItem(todoItem) {
     todoInput.value = '';
 };
 
+listContainer.addEventListener('click', (e) => {
+    if(e.target.classList.contains('delete')){
+        e.target.parentElement.remove();
+        checkEmptyList();
+    } else if(e.target.classList.contains('edit')){
+        const edit = e.target;
+        const text = e.target.parentElement.querySelector('.text');
+        const input = document.createElement('input');
+        input.value = text.textContent;
+        e.target.parentElement.replaceChild(input, text);
+        edit.textContent = '💾';
+        edit.classList.remove('edit')
+        edit.classList.add('save');
+    } 
+    else if(e.target.classList.contains('save')){
+        const save = e.target;
+        const span = document.createElement('span');
+        const text = e.target.parentElement.querySelector('input');
+        span.className = 'text';
+        span.textContent = text.value;
+        e.target.parentElement.replaceChild(span,text);
+        save.textContent = '✏️';
+        save.classList.remove('save');
+        save.classList.add('edit');
+    }
+})
+
+
+
 initialItems.forEach( (item) => {
     addTodoItem(item);
 });
+
+checkEmptyList()
